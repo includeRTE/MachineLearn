b55dad00975277cd537c61c9d1a7388b170af5cc
inlcude <119576534@qq.com>
2017-08-21 20:23:16 +0800

    modify adaboost.py

--- adaboost.py
+++ adaboost.py
@@ -77,6 +77,15 @@ def adaClassify(dataToClass,classifierArr):
 def loadDataSet(fileName):
     numFeat = len(open(fileName).readline().split('\t'))
     dataMat = []; labelMat = []
+    fr = open(fileName)
+    for line in fr.readlines():
+        lineArr = []
+        curLine = line.strip().split('\t')
+        for i in range(numFeat - 1):
+            lineArr.append(float(curLine[i]))
+        dataMat.append(lineArr)
+        labelMat.append(float(curLine[-1]))
+    return dataMat,labelMat
     
 if __name__ == '__main__':
     dataMat,classLabels = loadSimpData()
