3188d5f0155175be025b03ccd7fdf63167c43a1b
inlcude <119576534@qq.com>
2017-08-21 19:20:47 +0800

    modify adaboost.py

--- adaboost.py
+++ adaboost.py
@@ -41,8 +41,28 @@ def buildStump(dataArr,classLabels,D):
                     bestStump['thresh'] = threshVal
                     bestStump['ineq'] = inequal
     return bestStump,minError,bestClassEst
-
+def adaBoostTrainDS(dataArr,classLabels,numIt = 40):
+    weakClassArr = []
+    m = shape(dataArr)[0]
+    D = mat(ones((m,1)) / m)
+    aggClassEst = mat(zeros((m,1)))
+    for i in range(numIt):
+        bestStump,error,classEst = buildStump(dataArr,classLabels,D)
+        print("D:",D.T)
+        alpha = float(0.5*log((1.0-error)/max(error,1e-16)))
+        bestStump['alpha'] = alpha
+        weakClassArr.append(bestStump)
+        print("classEst:",classEst.T)
+        expon = multiply(-1*alpha*mat(classLabels).T,classEst)
+        D = multiply(D,exp(expon))
+        D = D / D.sum()
+        aggClassEst += alpha * classEst
+        print("aggClassEst:",aggClassEst.T)
+        aggErrors = multiply(sign(aggClassEst) != mat(classLabels).T,ones((m,1)))
+        errorRate = aggErrors.sum() / m
+        print("total error:",errorRate,"\n")
+        if errorRate == 0.0: break
+    return weakClassArr
 if __name__ == '__main__':
     dataMat,classLabels = loadSimpData()
-    D = mat(ones((5,1)) / 5)
-    buildStump(dataMat,classLabels,D)
+    classifierArray = adaBoostTrainDS(dataMat,classLabels,9)
