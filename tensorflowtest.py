3c30f00bf9ce03096e045ca29ad19597d13eacc7
inlcude <119576534@qq.com>
2017-08-25 10:25:25 +0800

    modify tensorflowtest.py

--- tensorflowtest.py
+++ tensorflowtest.py
@@ -4,24 +4,22 @@ import tensorflow as tf
 
 g1 = tf.Graph()
 with g1.as_default():
-    _initializer = tf.zeros_initializer()
-    _initializer.shape = [1]
-    v = tf.get_variable("v",initializer=_initializer)
+    v = tf.get_variable("v",shape=[1],initializer=tf.zeros_initializer())
 
 g2 =  tf.Graph()
 with g2.as_default():
-    v = tf.get_variable("v",initializer=tf.zeros_initializer(shape=[1]))
+    v = tf.get_variable("v",shape=[1],initializer=tf.ones_initializer())
 
 with tf.Session(graph=g1) as sess:
     tf.initialize_all_variables().run()
-    with tf.variable_scope("",ersuse=True):
+    with tf.variable_scope("",reuse=True):
         print(sess.run(tf.get_variable("v")))
 
 with tf.Session(graph=g2) as sess:
     tf.initialize_all_variables().run()
-    with tf.variable_scope("",ersuse=True):
+    with tf.variable_scope("",reuse=True):
         print(sess.run(tf.get_variable("v")))
 
-g = tf.Graph()
+'''g = tf.Graph()
 with g.device(None):
-    result = a + b
+    result = a + b'''
