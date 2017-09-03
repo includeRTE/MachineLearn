48e27a695b8faeac82e5fa40784b9d63e6409543
黄伟 <include@huangweideMacBook-Air.local>
2017-09-03 22:35:05 +0800

    modify tesnsorflow

--- tensorflowtest.py
+++ tensorflowtest.py
@@ -1,51 +1,46 @@
 import numpy as np
 from numpy import *
+from  numpy.random import RandomState
 import tensorflow as tf
 
-g1 = tf.Graph()
-with g1.as_default():
-    v = tf.get_variable("v",shape=[1],initializer=tf.zeros_initializer())
-
-g2 =  tf.Graph()
-with g2.as_default():
-    v = tf.get_variable("v",shape=[1],initializer=tf.ones_initializer())
-
-with tf.Session(graph=g1) as sess:
-    tf.initialize_all_variables().run()
-    with tf.variable_scope("",reuse=True):
-        print(sess.run(tf.get_variable("v")))
-
-with tf.Session(graph=g2) as sess:
-    tf.initialize_all_variables().run()
-    with tf.variable_scope("",reuse=True):
-        print(sess.run(tf.get_variable("v")))
-
+batch_size = 8
 w1 = tf.Variable(tf.random_normal([2,3],stddev=1,seed=1))
 w2 = tf.Variable(tf.random_normal([3,1],stddev=1,seed=1))
 
-x=tf.constant([[0.7,0.9]])
+x = tf.placeholder(tf.float32,shape=(None,2),name='x-input')
+y_ = tf.placeholder(tf.float32,shape=(None,1),name='y-input')
+
 a = tf.matmul(x,w1)
 y = tf.matmul(a,w2)
 
-sess = tf.Session()
-init_op = tf.initialize_all_variables()
-print(sess.run(init_op))
-sess.close()
-class TensorFlowFirst():
-    def __init__(self):
-        self.sess = tf.Session()
-        self.batch_size = 8
-        self.w1 = tf.Variable(tf.random_normal([2,3],stddev=1,seed=1))
-        self.w2 = tf.Variable(tf.random_normal([3,1],stddev=1,seed=1))
-        self.x = tf.placeholder(tf.float32,shape=(None,2),name='x-input')
-        self.y_ = tf.placeholder(tf.float32,shape=(None,1),name='y-input')
-        self.a = tf.matmul(x,w1)
-        self.y_ = tf.matmul(a,w2)
-        self.cross_entropy = -tf.reduce_mean(y_ * tf.log(tf.clip_by_value(y,1e-10,1.0)))
-        self.train_step = tf.train.AdamOptimizer(0.001).minimize(self.cross_entropy)
-        self.rdm  = RandomState(1)
-        self.data_size = 128
-        
+cross_entropy = -tf.reduce_mean(y_*tf.log(tf.clip_by_value(y,1e-10,1.0)))
+train_step = tf.train.AdamOptimizer(0.001).minimize(cross_entropy)
+
+rdm = RandomState(1)
+dataset_size = 128
+X = rdm.rand(dataset_size,2)
+Y = [[int(x1+x2 < 1)] for (x1,x2) in X]
+
+with tf.Session() as sess:
+    train_writer = tf.summary.FileWriter('/tmp/mnist_logs',sess.graph)
+    init_op = tf.initialize_all_variables()
+    sess.run(init_op)
+    print(sess.run(w1))
+    print(sess.run(w2))
+
+    STEPS = 5000
+    for i in range(STEPS):
+        start = (i * batch_size) % dataset_size
+        end = min(start + batch_size,dataset_size)
+
+        sess.run(train_step,feed_dict={x:X[start:end],y_:Y[start:end]})
+        print(sess.run(w1))
+        print(sess.run(w2))
+        if i % 1000 == 0:
+            total_cross_entropy = sess.run(cross_entropy,feed_dict={x:X,y_:Y})
+            print("After %d training_step(s),cross entropy on all data is %g" % (i,total_cross_entropy))
+    print(sess.run(w1))
+    print(sess.run(w2))
     
 '''g = tf.Graph()
 with g.device(None):
